27e1ca26013617f208c9762e1cb3f7a6794def89
terazan22-dot <terazan22@gmail.com>
2026-04-05 18:26:58 -0500

    Add files via upload

---
+++ 03-script.js
@@ -0,0 +1,10 @@
+console.log("Página cargada correctamente");
+
+// BOTON WHATSAPP REAL
+const boton = document.querySelector(".boton");
+
+boton.addEventListener("click", function(){
+
+    window.open("https://wa.me/573126398971","_blank");
+
+});
