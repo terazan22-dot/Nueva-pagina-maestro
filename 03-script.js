2ddd13a7b698b6eac13b14efc3d9ffbf77252f6a
terazan22-dot <terazan22@gmail.com>
2026-04-07 11:22:57 -0500

    Update 03-script.js

--- 03-script.js
+++ 03-script.js
@@ -8,3 +8,12 @@ boton.addEventListener("click", function(){
     window.open("https://wa.me/573126398971","_blank");
 
 });
+
+// Seleccionamos el botón hamburguesa y el menú
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+// Escuchamos el clic
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active'); // muestra/oculta el menú
+});
